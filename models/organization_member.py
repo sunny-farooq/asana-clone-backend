5f8cb7086d2fa573a9309602660893b348a0aa14
Sunny Farooq <msaneefarooq34@yahoo.com>
2026-03-03 09:29:44 +0500

    another migration

--- models/organization_member.py
+++ models/organization_member.py
@@ -1,11 +1,17 @@
 from tortoise.models import Model
 from tortoise import fields
+from enum import Enum
+
+class UserRole(str, Enum):
+    ADMIN = "admin"
+    USER = "user"
 
 class organization_member(Model):
     id = fields.UUIDField(pk = True)
     organization = fields.ForeignKeyField('models.organization', related_name="Organization", on_delete=fields.OnDelete.CASCADE, null=True)
     account = fields.ForeignKeyField('models.account', related_name='Organization Member', on_delete=fields.OnDelete.CASCADE)
-    role = fields.CharField(max_length=40)
+    role = fields.CharEnumField(UserRole, default=UserRole.USER)
+    invitation_code = fields.CharField(max_length=6, null= True)
     joined_at= fields.DatetimeField(auto_now_add=True)
 
     class Meta:
